6f0a66236f4a141ea51fb032249798fd7e37da91
Steel710 <adarshagrawal777@gmail.com>
2019-02-06 00:22:32 +0530

    database is now connected to the server and it is working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var bodyparse = require('body-parser');
 var app = express();
+var mongo = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 app.use(express.static('main_files'));
 
@@ -16,6 +18,32 @@ app.post('/login',function(req,res){
     var lgurl = req.body.url;
     console.log("the long url is: "+lgurl);
     res.end(lgurl);
+    //var data = {}; //with insertMany() method, many entries can be inserted in an array form eg: data = [{a,b},{a,b},{a,b},{a,b},{a,b}];.
+
+    //To connect the MongoDB database 
+    mongo.connect(url,{useNewUrlParser: true}, function(err, mongo) {
+        if (err) throw err;
+        var dbo = mongo.db("MainDB");
+        var data = {url: lgurl};
+        //To find the url in the DB
+        dbo.collection("URLs").find({url: lgurl}).toArray(function(err, result) {
+            if (err){
+                return err;
+            }
+            else if(result){
+                console.log("URL is already present at id "+result[0]._id);
+            }
+            else{
+                //To insert url into the DB
+                dbo.collection("URLs").insertOne(data, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document inserted");
+                });
+            }
+            mongo.close();
+        });
+    });
+
 });
 
 app.listen(8080,function(){
